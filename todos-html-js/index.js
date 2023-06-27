6db9cf29cd408a40ff581fff21ae8fe7a7932f26
Ego Maragustaf <egomaragustaf@gmail.com>
2023-06-27 22:47:26 +0700

    add delete a task action button and checkbox

--- todos-html-js/index.js
+++ todos-html-js/index.js
@@ -1,14 +1,30 @@
 const todoFormElement = document.getElementById("todo-form");
 const todoInputElement = document.getElementById("todo-input");
 const todoListElement = document.getElementById("todo-list");
+const clearCompletedTask = document.getElementById("delete-completed-task");
 
 todoFormElement.onsubmit = function (event) {
   event.preventDefault();
 
   const todoInputValue = todoInputElement.value;
-  const listValue = document.createElement("li");
+  const listValue = document.createElement("div");
   const listText = document.createTextNode(todoInputValue);
+  const checkBoxValue = document.createElement("input");
 
+  listValue.classList.add("list");
+  checkBoxValue.type = "checkbox";
+  checkBoxValue.classList.add("toggle");
+  listValue.appendChild(checkBoxValue);
   listValue.appendChild(listText);
   todoListElement.appendChild(listValue);
+
+  const deleteButton = document.createElement("button");
+
+  deleteButton.classList.add("delete");
+  deleteButton.innerText = "Delete";
+  listValue.appendChild(deleteButton);
+
+  deleteButton.addEventListener("click", () => {
+    todoListElement.removeChild(listValue);
+  });
 };
